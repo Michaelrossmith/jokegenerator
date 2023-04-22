db6cc0a3ea7a3c75105e712e47d7fd999ef2127d
Reece Coetzer <reececoetzer@Reeces-MacBook-Pro-2.local>
2023-04-22 10:04:03 +0200

    Code body added

---
+++ scripts.js
@@ -0,0 +1,9 @@
+﻿const nouns = ["cat", "pet bird", "chair", "uncle", "forearm", "money", "dining table", "chess board"];
+const verbs = ["eats", "hears", "drinks", "vomits", "tailgates", "annoys me", "switches on"];
+const adjectives = ["loudly", "brightly", "vividly", "powerfully", "fast", "hungrily"];
+
+const pickRandomItem = (array) => {
+    return array[Math.floor((Math.random() * array.length))];
+};
+
+console.log(`My ${pickRandomItem(nouns)} ${pickRandomItem(verbs)} ${pickRandomItem(adjectives)}`);
